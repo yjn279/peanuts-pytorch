894c44905d32af0a88084a5461e89e3cd122106e
winnie279 <57951904+Yuji-207@users.noreply.github.com>
2025-06-13 04:18:24 +0900

    fix: Update test configuration to use the correct CSV file for testing and ensure predictions are exported properly in output function

--- src/peanuts/utils/output_fn.py
+++ src/peanuts/utils/output_fn.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+from ..output.export_predictions import export_predictions
 from ..output.plot_diffs import plot_diffs
 from ..output.plot_event import plot_event
 from .misc import get_device, get_diffs
@@ -40,7 +41,7 @@ def output_fn(
                     path=f"plots/waveforms/{path_event}.eps",
                 )
 
-                # export_predictions(path_event, pred_event)
+                export_predictions(path_event, pred_event)
 
                 diffs_p += get_diffs(pred_event[1], y_event[1], mph, mpd)
                 diffs_s += get_diffs(pred_event[2], y_event[2], mph, mpd)
